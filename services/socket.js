ac6368413cb7f89c68543e1f901f2d0877d0b01d
nikitha132 <71176001+nikitha132@users.noreply.github.com>
2024-01-28 21:42:30 +0530

    add conversation to ticket

--- services/socket.js
+++ services/socket.js
@@ -1,5 +1,6 @@
 // Use import syntax for ECMAScript modules
 import { Server } from 'socket.io';
+import Ticket from '../models/ticket.model.js';
 
 export default function initializeSocket(server) {
 
@@ -11,10 +12,34 @@ export default function initializeSocket(server) {
         console.log(`User connected: ${socket.id}`);
 
         // Handle when a user sends a chat message
-        socket.on('chatMessage', (data) => {
-            console.log("Data", data);
-            // Broadcast the message to all connected clients
-            socket.emit('chatMessage', data);
+        socket.on('chatMessage', async (data) => {
+
+           
+            const { ticketId , message , userId } = JSON.parse(data) ;
+
+            const conversation = {
+                sentBy: userId,
+                message,
+            }
+
+            try {
+                const updatedTicket = await Ticket.findOneAndUpdate(
+                    { ticketId: ticketId },
+                    { $push: { conversations: conversation } },
+                    { new: true } // Return the modified document rather than the original
+                );
+
+                if (!updatedTicket) {
+                    console.error(`Ticket with ticketId ${ticketId} not found.`);
+                    return;
+                }
+
+                // Broadcast the message to all connected clients
+                io.emit('chatMessage', data);
+            } catch (error) {
+                console.error('Error updating conversations:', error.message);
+                // Handle the error as needed
+            }
         });
 
         // Handle when a user disconnects
